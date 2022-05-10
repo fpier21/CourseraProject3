7f1cf212cddd7dc34791c7475ffa65013facf0e8
fpier21 <franceschiello20@gmail.com>
2022-05-10 16:08:59 +0200

    misundersting about the indication 'extracting columns', as previously this was into a different dataset

--- run_analysis.R
+++ run_analysis.R
@@ -3,31 +3,38 @@
 
 library(dplyr)
 
-#download files 
+#downloading files 
 
 filename <- "Coureproject.zip"
 
-# Checking if archieve already exists.
+# checking if the archieve already exists
 if (!file.exists(filename)){
         fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
         download.file(fileURL, filename, method="curl")
 }  
 
-# Checking if folder exists
+# checking if folder already exists
 if (!file.exists("UCI HAR Dataset")) { 
         unzip(filename) 
 }
 
-# importing files 
+# importing files
 
-test <- read.table('UCI HAR Dataset/test/X_test.txt', sep= '')
-test_labels <- read.table('UCI HAR Dataset/test/y_test.txt', sep ='', col.names = 'activity')
-train <- read.table('UCI HAR Dataset/train/X_train.txt', sep = '')
-train_labels <- read.table('UCI HAR Dataset/train/y_train.txt', sep = '', col.names = 'activity')
-conversion <- read.table('UCI HAR Dataset/activity_labels.txt', sep = '')
+# making unique names for the features 
+
 features <- read.table('UCI HAR Dataset/features.txt', sep = '')
+features$V2 <- make.names(features$V2, unique = T) 
+
+test <- read.table('UCI HAR Dataset/test/X_test.txt', sep= '', col.names = features$V2)
+test_labels <- read.table('UCI HAR Dataset/test/y_test.txt', sep ='', col.names = 'activity')
+train <- read.table('UCI HAR Dataset/train/X_train.txt', sep = '', col.names = features$V2)
+train_labels <- read.table('UCI HAR Dataset/train/y_train.txt', sep = '', col.names = 'activity')
+
+conversion <- read.table('UCI HAR Dataset/activity_labels.txt', sep = '')
+
 subject_train <- read.table('UCI HAR Dataset/train/subject_train.txt', sep ='', col.names = 'subject')
 subject_test <- read.table('UCI HAR Dataset/test/subject_test.txt', sep ='', col.names = 'subject')
+
 # merging the data sets
 
 test <- cbind(test,test_labels,subject_test)
@@ -35,12 +42,11 @@ train <- cbind(train,train_labels,subject_train)
 
 mer_data <- rbind(test,train)
 
-# extracting only columns relating to mean and and standard deviation in a new data frame
+# extracting only columns relating to mean and and standard deviation
 
-#making unique features' names
-features_names <- make.names(features[,2], unique = T)
+features_names <- features$V2[grepl('mean()', features$V2) | grepl('std()', features$V2)]
+mer_data <- mer_data[,c(features_names, 'activity', 'subject')]
 
-mean_std_dataset <- mer_data[,grepl('mean()', features_names) | grepl('std()', features_names)]
 
 # descriptive activity names
 
@@ -59,6 +65,4 @@ names(mer_data) <- c(features_names, 'activity', 'subject')
 # new tidy data set
 
 tidy_data <- summarize_all(group_by(mer_data, activity, subject), mean)
-
-
-
+write.table(tidy_data, row.names = F, file = 'tidy_data_set.txt')
